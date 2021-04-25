81594aeae272cb589b98c6464d13f10404a85750
BossBele <adambeleko@gmail.com>
2021-04-25 18:24:06 +0300

    version and title function tests

--- lingo.test.js
+++ lingo.test.js
@@ -1,4 +1,10 @@
 const lingo = require("./lingo");
-test("Returns about-us for english language", () => {
-  expect(getAboutUsLink("en-US")).toBe("/about-us");
+test("Returns lingo version", () => {
+  expect(lingo.version).toBeTruthy();
+  expect(typeof lingo.version).toBe("string");
+});
+test("Converts string to title case", () => {
+  console.log(lingo(), lingo().title());
+  expect(typeof lingo().title()).toBe("string");
+  expect(lingo("lingo  js-is cool").title()).toBe("Lingo  Js-is Cool");
 });
